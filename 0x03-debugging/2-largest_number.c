e286807c912b2a59e63e1d4883e96a9ab7775cea
Albert Yankey <nanakwameyankey24@gmail.com>
2022-09-15 04:29:00 -0700

    Task 1

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,6 +7,7 @@
  * @c: third number
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -15,17 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > c && b > a)
+	else if (b > c)
 	{
 		largest = b;
-	}
-	else if (c > b)
-	{
-		largest = c;
 	}
 	else
 	{
-		largest = b;
+		largest = c;
 	}
 
 	return (largest);
